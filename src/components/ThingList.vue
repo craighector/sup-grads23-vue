7b112b1cb3b38dcc0fe8532778627ed3686a3e0f
Ed Dikotope <ed.dikotope@superbalist.com>
2022-02-17 16:11:23 +0200

    add ThingList component

---
+++ src/components/ThingList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button>Refresh</button>
+        <div>
+            <p>A thing</p>
+            <button>Remove</button>
+            <button>Flag</button>
+        </div>
+        <div>
+            <p>There are no things.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ThingList",
+    data() {
+        return {
+            things: []
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
